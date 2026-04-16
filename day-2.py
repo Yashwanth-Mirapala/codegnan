d957f7de1ad608c04c8e848738ad369d0f7dbbc8
Yashwanth-Mirapala <yeshumirapala@gmail.com>
2026-04-16 10:30:51 +0530

    Add files via upload

---
+++ day-2.py
@@ -0,0 +1,103 @@
+
+'''
+Variable :
+
+--> To define a variable , we have 2 rules
+
+
+1. good way to define a variable (No Error)
+
+
+Example
+
+N=8
+print(N)
+
+
+_f=4
+print(_f)
+
+
+d=8
+print(d)
+
+
+2. bad way to define a variable ( Error)
+ if we use Number or digits
+ ife we use special charecters
+ incliding space
+ if we use space b/w the variable it shows error
+
+Example
+
+
+@n =7
+print(@n )
+
+ n um=9
+
+
+--> Note we are going to use meaningfull words or names to defining variables
+
+Example
+
+num_1=20
+num_2=10
+
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+KEYWORDS :(Also called as Keywords)
+
+--> these keywords not going to use as a variable
+Example-
+
+ if,else,for,while,elif,....etc
+---------------------------------------------------------------------------------------------------------------------------------- 
+a,b,c=23,22,21
+print(a,b,c)
+
+print(a)
+print(b)
+print(c)
+----------------------------------------------------------------------------------------------------------------------------------
+TOKEN :
+--> IS nottiing but a small program (or)code in python to complete the task
+Example-
+
+a,b,c=23,22,21
+print(a+b+c)
+----------------------------------------------------------------------------------------------------------------------------------
+Comments :
+-->there are two types of comments
+
+ --> Single line comment
+ : This is used to explain about the particular line iin the code, for this we can use '#' symbole
+ 
+ --> Multi-line comment
+ : To comment more than single line we can use multi-line coommenting ,for this we can use
+ """ """ (or) ''' '''
+ any thing that is in the middle of the comments that wont be considerd by the cursor
+-----------------------------------------------------------------------------------------------------------------------------------
+BOOLEN TYPE :
+--> This is used to find ot  the statement is true of false
+Example :
+num_1=90
+num_2=9
+print(num_1==num_2)
+-----------------------------------------------------------------------------------------------------------------------------------
+Even or Odd numbers :
+
+num=7
+if num%2==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
+    
+'''
+
+
+
+
+
+
