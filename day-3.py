606e992be8a4b40072427fe508b9bc8bcd7bb93b
Yashwanth-Mirapala <yeshumirapala@gmail.com>
2026-04-17 10:55:45 +0530

    day-3 of learning OPERATORS

---
+++ day-3.py
@@ -0,0 +1,181 @@
+'''
+Operators
+-----------------------------
+1.Arthametic operator
+2.Assignment operator
+3.Coomparision operator
+4.Logical operator
+5.Identity operator
+6:Membership operator
+7.Bitwise operator
+-----------------------------------------------------------
+1.Arthametic operator --> +,-,%,*,**
+Ex:
+Num_1=20
+Num_2=30
+Num_3=40
+print(Num_1+Num_2+Num_3)
+o/p--> 90
+Ex:
+num=4
+num_2=3
+print(num - num_2)
+..o/p-->1
+Ex:
+a=8
+b=3
+print(a % b)
+ex:
+digi_1=70
+digi_2=90
+print(digi_1 * digi_2)
+o/p..>6300
+ex:
+digi_1=7
+digi_2=9
+print(digi_1 ** digi_2)
+o/p-->40353607
+ex:
+num=4.4 #floor division is used to round upthe decimal value
+num_2=3.7
+print(num //num_2)
+o/p-->1.0
+-----------------------------------------------------------------
+2.Assignment operator --> =,+=,-=,%=,//=,*=,**=,
+--> (=) --> to assign the Datatype to the variable
+ex:+=
+number=12
+number +=10
+print(number)
+
+ex:-=
+minus=12
+minus -=10
+print(minus)
+ex:*=
+star=12
+star *=10
+print(star)
+
+ex:**=
+pwr=12
+pwr **=10
+print(pwr)
+----------------------------------------------------
+3.Coomparision operator--> ==,!=,>'<,>=,<=,
+
+Ex=
+
+
+Lhs=20
+RHs=30
+print(Lhs==RHs)
+
+Lhs=20
+RHs=30
+print(Lhs!=RHs)
+
+
+Lhs=20
+RHs=30
+print(Lhs>RHs)
+
+Lhs=20
+RHs=30
+print(Lhs<RHs)
+
+Lhs=20
+RHs=30
+print(Lhs<=RHs)
+
+Lhs=20
+RHs=30
+print(Lhs>=RHs)
+-----------------------------------------------------------
+4.Logical operator-->
+
+and --> the conditions should be true 
+Lhs=20
+RHs=30
+print(Lhs>22 and RHs<10)
+#false
+print(Lhs<22 and RHs>10)
+#true
+
+or--> any one condition must be true
+
+Lhs=20
+RHs=30
+print(Lhs>22 or RHs>10)
+#true
+
+not--> used to reverse the condition
+--------------------------------------------------------------
+5.Identity operator--> is , isnot
+is--> this is used  to check the object(Note-diff b/w is & ==)
+Ex:
+
+y=9
+z=9
+print(y is z)
+print(id(y))
+print(id(z))
+
+list_=[1,2]
+list_2=[1,2]
+list_3=list_
+print(list_ is list_3)
+print(id(list_))
+print(id(list_3))
+
+is not--> to check is the operator is not same
+
+Ex:
+list_=[1,2]
+list_2=[1,2]
+print(list_ is not list_2)
+--------------------------------------------------------------
+6:Membership operator-->
+in --> is used to check , it is present in it 
+list_=[1,2]
+print(2 in list_)
+not in-->  is used to check weather itis not there
+--------------------------------------------------------------
+7.Bitwise operator : &, | ,^,~
+&--> bitwise AND
+ Ex: print(5&4)
+
+^--> bitwise XOR
+EX:
+print (5^3)
+
+| --> bitwise OR
+
+'''
+print (5|3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
