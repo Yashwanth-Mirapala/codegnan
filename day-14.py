30ab32b1235fa4ff23ebf41d3b32a3fe336f1a38
Yashwanth-Mirapala <yeshumirapala@gmail.com>
2026-05-18 09:35:13 +0530

    day -14 of learning python

---
+++ day-14.py
@@ -0,0 +1,43 @@
+'''
+ Modules
+ --------
+-->A module is a file containing python code such as :
+-Varables
+-functions
+-classes
+
+Types of MOdules
+-----------------
+--> User define modules
+--> In built modules
+---> built in modules
+->os
+->math
+math module
+---------------
+-->> this modukle is used to do mathmetical operations
+import math
+print(math.(49))
+
+
+->sys
+--> sys module is a system-specific parameter nd functions.
+ex:
+import sys
+print(sys.version)
+
+---> To use all this modules,we have to import with module name
+ways to import modules
+-----------------------
+1. using Alias NAme
+2.import entire module
+3. import all functions
+import specific function
+
+randome module
+---------------
+'''
+    
+
+
+
