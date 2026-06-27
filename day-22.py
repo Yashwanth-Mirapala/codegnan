df1b23b457f3c5559bea5348277effa942ba1814
Yashwanth-Mirapala <yeshumirapala@gmail.com>
2026-06-27 16:52:50 +0530

    day-22 of learning python

---
+++ day-22.py
@@ -0,0 +1,54 @@
+"""
+Data Analysis
+----->DA is the process of inspecting,cleaning,transforming,modelling data to discover useful insights,support decision-making...
+Types of Analysis
+----------
+1)Descriptive Analysis--->Summarizing Data
+2)Diagnostic Analysis---->Understanding cause such as sales dropped
+3)Predictive Analysis---->Forecasting future outcomes
+4)Prescriptive Analysis-->Suggesting actions based on data(marketing)
+
+Numpy (ndarray)
+---->Numerical Python (Numpy)
+---->It provides support for multi-dimensional arrays
+
+creating Numpy arrays
+-------------------
+import numpy as np
+arr=np.array([1,3,4,5])
+print(arr)
+#2-dimensional array
+arr_2=np.array([[1,2,3],[4,5,6]])
+print(arr_2)
+
+#3-dimensional array
+import numpy as np
+arr_3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr_3)
+
+arrange(start,end,step)
+
+import numpy as np
+arr=np.arange(1,20,2)
+print(arr)
+
+import numpy as np
+arr_3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+reshap=arr_3.reshape(3,2)
+print(reshap)
+
+
+.mean()
+.sum()
+.max()
+.min()
+.head()
+.tail()
+
+"""
+ 
+
+
+
+
+
